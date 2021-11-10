c5338e5304563d50204c6ace921cf97541d96398
B. Kerem KURT <kkurt@sabanciuniv.edu>
2021-11-10 10:26:35 +0300

    rectangle

--- main.cpp
+++ main.cpp
@@ -3,15 +3,17 @@
 Vertex vertices[] =
 {
 	//Position							//Color								//Texcoords
-	glm::vec3(0.0f, 0.5f, 0.f),			glm::vec3(1.f, 0.f, 0.f),			glm::vec2(0.f, 1.f),
+	glm::vec3(-0.5f, 0.5f, 0.f),		glm::vec3(1.f, 0.f, 0.f),			glm::vec2(0.f, 1.f),
 	glm::vec3(-0.5f, -0.5f, 0.f),		glm::vec3(0.f, 1.f, 0.f),			glm::vec2(0.f, 0.f),
 	glm::vec3(0.5f, -0.5f, 0.f),		glm::vec3(0.f, 0.f, 1.f),			glm::vec2(1.f, 0.f),
+	glm::vec3(0.5f, 0.5f, 0.f),			glm::vec3(1.f, 1.f, 0.f),			glm::vec2(0.f, 0.f)
 };
 unsigned nrOfVertices = sizeof(vertices) / sizeof(Vertex);
 
 GLuint indices[] =
 {
-	0, 1, 2
+	0, 1, 2, //Triangle 1
+	0, 2, 3 //Triangle 2
 };
 unsigned nrOfIndices = sizeof(indices) / sizeof(GLuint);
 
